00e1b6ee5b64924e97c0079fc13d5dfb5819b9af
Mukul Agarwal <agarmukul23@gmail.com>
2023-03-12 01:10:45 -0600

    tweak nvim settings

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -3,5 +3,22 @@
     programs.neovim = {
         enable = true;
         coc = { enable = true; };
+        defaultEditor = true;
+        plugins = with pkgs.vimPlugins; [
+            yankring
+            vim-nix
+            lightline-vim
+            dracula-nvim
+            nerdtree
+            vim-commentary
+            fzf-vim
+            vim-startuptime
+            {
+                plugin = dashboard-nvim;
+            }
+        ];
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
     };
 }
