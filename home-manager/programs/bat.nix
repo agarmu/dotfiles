e7dea6a9ed573ff17140bd22cb3b23cee0c28673
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:37:20 -0400

    change bat theme to catppuccin-machiatto

--- home-manager/programs/bat.nix
+++ home-manager/programs/bat.nix
@@ -1,18 +1,21 @@
-{ config, pkgs, ... }:
-let draculatheme = builtins.fetchurl {
-	url = "https://raw.githubusercontent.com/dracula/sublime/c5de15a0ad654a2c7d8f086ae67c2c77fda07c5f/Dracula.tmTheme";
-	sha256 = "0nlapygw0jwv1gvg9mkb47n3v6hq1i86y587h889sxd2ymygwrv7";
-}; in
 {
-	programs.bat = {
-		enable = true;
-		extraPackages = with pkgs.bat-extras; [ batgrep batman batpipe batwatch batdiff prettybat ];
-		config = {
-			theme = "dracula";
-		};
-		themes = {
-			dracula = builtins.readFile (draculatheme);
-		};
-	};
+  config,
+  pkgs,
+  ...
+}: let
+  draculatheme = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/bat/ba4d16880d63e656acced2b7d4e034e4a93f74b1/Catppuccin-macchiato.tmTheme";
+    sha256 = "0ydk3ysvxgdy829jfl2bwhzhwv3kh5d4k2l6047bi8dwbbid59zg";
+  };
+in {
+  programs.bat = {
+    enable = true;
+    extraPackages = with pkgs.bat-extras; [batgrep batman batpipe batwatch batdiff prettybat];
+    config = {
+      theme = "Catppuccin-macchiato";
+    };
+    themes = {
+      dracula = builtins.readFile draculatheme;
+    };
+  };
 }
-
