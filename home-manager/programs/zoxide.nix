b12b50b61b7d34424cbd16b8596e54a793f14302
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:30:03 -0400

    add zoxide

---
+++ home-manager/programs/zoxide.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+    enableZshIntegration = true;
+  };
+}
