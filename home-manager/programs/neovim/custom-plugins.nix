f8ad62c683589aab7a470df276f07d61311f4218
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:38:51 -0400

    fix neovim: theme, mason

---
+++ home-manager/programs/neovim/custom-plugins.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  catppuccin = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    name = "github";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "nvim";
+      rev = "b573866ef64122119668f375ebb794bcb3b518e2";
+      hash = "sha256-9401fBGF1up2wVaNZIdLW408/lfMdh8wBabaUcc6oqc=";
+
+    };
+  };
+}
