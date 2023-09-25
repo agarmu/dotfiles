d8bb723bcb421d0621a3257226d1836a27e48e08
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:45:43 -0400

    organize mas dependencies

--- nix-darwin/homebrew.nix
+++ nix-darwin/homebrew.nix
@@ -1,50 +1,60 @@
-{ config, pkgs, ... }:
 {
-	homebrew = {
-	    enable = true;
-	    taps = [
-		"homebrew/cask"
-	    ];
-	    brews = [
-              "openjdk"
-            ];
-	    casks = [
-		"thunderbird"
-		"anaconda"
-		"adobe-acrobat-pro"
-		"anki"
-		"calibre"
-		"docker"
-		"eagle"
-		"hammerspoon"
-		"inkscape" "gimp"
-		"insomnia"
-		"logseq"
-		"maccy"
-		"mochi"
-		"qbittorrent"
-		"raspberry-pi-imager"
-		"signal"
-		"skim"
-		"spotify"
-		"veracrypt"
-		"vlc"
-		"xournal-plus-plus"
-		"zoom"
-		"zotero"
-		"anaconda"
-              ];
+  config,
+  pkgs,
+  ...
+}: {
+  homebrew = {
+    enable = true;
+    taps = [
+      "homebrew/cask"
+    ];
+    brews = [
+      "openjdk"
+    ];
+    casks = [
+      "thunderbird"
+      "firefox"
+      "anaconda"
+      "adobe-acrobat-pro"
+      "anki"
+      "calibre"
+      "docker"
+      "eagle"
+      "hammerspoon"
+      "inkscape"
+      "gimp"
+      "insomnia"
+      "logseq"
+      "maccy"
+      "mochi"
+      "qbittorrent"
+      "raspberry-pi-imager"
+      "signal"
+      "skim"
+      "spotify"
+      "veracrypt"
+      "vlc"
+      "xournal-plus-plus"
+      "zoom"
+      "zotero"
+      "anaconda"
+    ];
 
-	    masApps = {
-		"GeoGebra Calculator Suite" = 1504416652;
-		"Microsoft OneNote" = 784801555;
-		"Microsoft Excel" = 462058435;
-		"Slack" = 803453959;
-		"Microsoft Outlook" = 985367838;
-		"WhatsApp" = 1147396723;
-		"Microsoft Word" = 462054704;
-		"Pages" = 409201541;
-		"Microsoft PowerPoint" = 462062816;
-	    };
-	};
+    masApps = {
+      # Microsoft Office
+      "Microsoft Excel" = 462058435;
+      "Microsoft Word" = 462054704;
+      "Microsoft PowerPoint" = 462062816;
+
+      # Apple Office
+      "Pages" = 409201541;
+
+      # Communications
+      "Slack" = 803453959;
+      "WhatsApp" = 1147396723;
+
+      # Miscellaneous
+      "GeoGebra Calculator Suite" = 1504416652;
+    };
+  };
 }
