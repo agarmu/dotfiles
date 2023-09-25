14eefdd6f66c3c19cf7abfee8e410a4c8ecbda18
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 19:20:54 -0400

    fix: bat theme

--- home-manager/programs/bat.nix
+++ home-manager/programs/bat.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: let
-  draculatheme = builtins.fetchurl {
+  catppuccin-macchiato-file = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/catppuccin/bat/ba4d16880d63e656acced2b7d4e034e4a93f74b1/Catppuccin-macchiato.tmTheme";
     sha256 = "0ydk3ysvxgdy829jfl2bwhzhwv3kh5d4k2l6047bi8dwbbid59zg";
   };
@@ -12,10 +12,10 @@ in {
     enable = true;
     extraPackages = with pkgs.bat-extras; [batgrep batman batpipe batwatch batdiff prettybat];
     config = {
-      theme = "Catppuccin-macchiato";
+      theme = "catppuccin-macchiato";
     };
     themes = {
-      dracula = builtins.readFile draculatheme;
+      catppuccin-macchiato = builtins.readFile catppuccin-macchiato-file;
     };
   };
 }
