0fe5048f18ab0a4acc80049645615bc7bf4a49af
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:39:26 -0400

    fix shellInit for nix-darwin

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,16 +1,27 @@
-{ config, pkgs, ... }:
-let services = map (n: "${./services}/${n}") (builtins.attrNames (builtins.readDir ./services));
-	programs = map (n: "${./programs}/${n}") (builtins.attrNames (builtins.readDir ./programs));
-in
 {
-	imports = [
-	# System Configuration
-	./fonts.nix ./networking.nix ./nixconfig.nix ./packages.nix ./security.nix ./shells.nix
-	./homebrew.nix
-	] ++ services ++ programs;
-	# Used for backwards compatibility, please read the changelog before changing.
-	# $ darwin-rebuild changelog
-	system.stateVersion = 4;
-	environment.shellInit = ''eval "$(/usr/libexec/path_helper)"
-eval "$(/opt/homebrew/bin/brew shellenv)"'';
+  config,
+  pkgs,
+  ...
+}: let
+  services = map (n: "${./services}/${n}") (builtins.attrNames (builtins.readDir ./services));
+  programs = map (n: "${./programs}/${n}") (builtins.attrNames (builtins.readDir ./programs));
+in {
+  imports =
+    [
+      # System Configuration
+      ./fonts.nix
+      ./networking.nix
+      ./nixconfig.nix
+      ./packages.nix
+      ./security.nix
+      ./shells.nix
+      ./homebrew.nix
+    ]
+    ++ services
+    ++ programs;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+  environment.shellInit = ''    eval "$(/usr/libexec/path_helper)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"'';
 }
