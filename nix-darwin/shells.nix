9fb903ed884ac9d09c2b17bdcccf91687530bf96
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-25 18:42:11 -0400

    remove redundant zsh config in shells.nix

--- nix-darwin/shells.nix
+++ nix-darwin/shells.nix
@@ -1,18 +1,11 @@
-{ config, pkgs, ... }:
 {
-	environment.shells = 
-		with pkgs; [
-			bashInteractive
-			zsh
-			fish
-			nushell
-		];
-	# Create /etc/zshrc that loads the nix-darwin environment.
-	programs.zsh = {
-		enable = true;
-		enableCompletion = true;
-		enableFzfCompletion = true;
-		enableFzfGit = true;
-		enableSyntaxHighlighting = true;
-	};
+  config,
+  pkgs,
+  ...
+}: {
+  environment.shells = with pkgs; [
+    bashInteractive
+    zsh
+    nushell
+  ];
 }
