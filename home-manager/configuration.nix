fe45539d29bf06de3c40a7944dc7ab7cfde2f54d
Mukul Agarwal <agarmukul23@gmail.com>
2023-10-16 05:06:09 -0400

    add nur overlay

--- home-manager/configuration.nix
+++ home-manager/configuration.nix
@@ -22,7 +22,13 @@ in {
     imports = [./packages.nix] ++ programs ++ services;
     nixpkgs.config = {
       allowUnfree = true;
+      packageOverrides = pkgs: {
+        nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+          inherit pkgs;
+        };
+      };
     };
+
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
     home = {
