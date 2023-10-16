876cd5a7cb57d00841178f393ddb3435b916fc87
Mukul Agarwal <agarmukul23@gmail.com>
2023-10-16 05:08:12 -0400

    updated homebrew settings nix

--- nix-darwin/homebrew.nix
+++ nix-darwin/homebrew.nix
@@ -5,39 +5,48 @@
 }: {
   homebrew = {
     enable = true;
+    onActivation = {
+      cleanup = "zap";
+    };
     taps = [
       "homebrew/cask"
+      "oven-sh/bun"
     ];
     brews = [
       "openjdk"
+      "openssh"
+      "bun"
+      "llvm"
+      "exiftool"
+      "meson"
+      "pandoc"
+      "ykman"
     ];
     casks = [
-      "thunderbird"
-      "firefox"
       "anaconda"
-      "adobe-acrobat-pro"
       "anki"
       "calibre"
+      "coconutbattery"
       "docker"
-      "eagle"
+      "element"
+      "firefox"
+      "free-gpgmail"
+      "godot"
       "hammerspoon"
+      "handbrake"
       "inkscape"
-      "gimp"
-      "insomnia"
-      "logseq"
+      "kitty"
+      "livebook"
       "maccy"
-      "mochi"
       "qbittorrent"
-      "raspberry-pi-imager"
       "signal"
       "skim"
       "spotify"
-      "veracrypt"
+      "thunderbird"
       "vlc"
       "xournal-plus-plus"
       "zoom"
       "zotero"
-      "anaconda"
     ];
 
     masApps = {
@@ -45,16 +54,18 @@
       "Microsoft Excel" = 462058435;
       "Microsoft Word" = 462054704;
       "Microsoft PowerPoint" = 462062816;
+      "Onedrive" = 823766827;
 
       # Apple Office
       "Pages" = 409201541;
-
+      "Keynote" = 409183694;
       # Communications
       "Slack" = 803453959;
       "WhatsApp" = 1147396723;
 
       # Miscellaneous
       "GeoGebra Calculator Suite" = 1504416652;
+      "Tailscale" = 1475387142;
     };
   };
 }
