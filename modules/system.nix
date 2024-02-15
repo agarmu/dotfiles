812a451d05db48f01ce7e5e46e24b6e8ef9ef0b2
Mukul Agarwal <agarmukul23@gmail.com>
2024-02-15 12:03:28 -0500

    enable keyboard remaps

--- modules/system.nix
+++ modules/system.nix
@@ -1,13 +1,15 @@
 {pkgs, ...}: {
   system = {
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    # TODO:
     activationScripts.postUserActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
-
+    
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
     defaults = {
       menuExtraClock.Show24Hour = true; # show 24 hour clock
       dock = {
