9698bd66c315721cccde8d7f79df23b371802ccb
Mukul Agarwal <agarmukul23@gmail.com>
2024-02-15 12:08:19 -0500

    add spell check

--- home/packages.nix
+++ home/packages.nix
@@ -7,6 +7,12 @@
 }: {
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
+    ############
+    # Spelling #
+    ############
+    aspell
+    aspellDicts.en
+    aspellDicts.en-computers
     ##################
     # Custom scripts #
     ##################
