97efc14e9fd62cc85f5a6c927c2a8c48a88bf442
Mukul Agarwal <agarmukul23@gmail.com>
2024-04-28 22:06:35 -0400

    bypass paywalls clean

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -12,6 +12,14 @@
     sha256 = "1c66d8fccbdc7096cfc5b4b5f250c4249fbe48395c1b944d2fd26d52434a2dbd";
     meta = {};
   };
+  bpc = buildFirefoxXpiAddon {
+    pname = "bypass-paywalls-clean";
+    version = "3.6.5.0";
+    addonId = "bpc@bpc.bpc";
+    url = "https://github.com/bpc-clone/bpc_updates/releases/download/latest/bypass_paywalls_clean-3.6.5.0.xpi";
+    sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+    meta = {};
+  };
 in {
   programs.firefox = {
     enable = true;
@@ -31,7 +39,7 @@ in {
         wayback-machine
         youtube-recommended-videos
         zotero-connector
-        bypass-paywalls-clean
+        bpc # TODO:bypass-paywalls-clean
         unpaywall
         sidebery
         profile-switcher
