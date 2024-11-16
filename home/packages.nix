eb749cf34eceb32c9ba0bdf7c80a53eb18502e1c
Mukul Agarwal <agarmukul23@gmail.com>
2024-11-16 17:43:47 -0500

    update packages.nix (comment formatting, add/remove packages, etc.)

--- home/packages.nix
+++ home/packages.nix
@@ -32,119 +32,120 @@
     │ Version Control │
     └─────────────────┘
     */
-    git                         # most popular vcs
+    git                             # most popular vcs
+    lazygit                         # nicer ui for git
 
     /*
     ┌─────────────┐
     │ Nix Tooling │
     └─────────────┘
     */
-    alejandra                   # Formatter for Nix
-    nixd                        # lsp
-    deadnix                     # dead code detection
-    statix                      # static analysis
+    alejandra                       # Formatter for Nix
+    nixd                            # lsp
+    deadnix                         # dead code detection
+    statix                          # static analysis
     /*
     ┌────────────┐
     │ Networking │
     └────────────┘
     */
-    openssh                     # SSH Agent
-    sshfs                       # Mount remote filesystems
-    aria                        # CLI download utility
-    socat                       # Better netcat
-    wget2                       # Web Get (v2)
-    curl                        # client for URL
+    openssh                         # SSH Agent
+    sshfs                           # Mount remote filesystems
+    aria                            # CLI download utility
+    socat                           # Better netcat
+    wget2                           # Web Get (v2)
+    curl                            # client for URL
+    rsync                           # fast remote sync
 
     /*
     ┌──────────────┐
     │ Multiplexers │
     └──────────────┘
     */
-    tmux                        # terminal multiplexer
+    tmux                            # terminal multiplexer
 
     /*
     ┌─────────────┐
     │ Compression │
     └─────────────┘
     */
-    zstd                        # fast real-time compression algo developed by facebook
-    brotli                      # losless compression
+    zstd                            # fast real-time compression algo developed by facebook
+    brotli                          # losless compression
 
     /*
     ┌─────────────────┐
     │ Tools/Utilities │
     └─────────────────┘
     */
-    delta                       # nice diff viewer
-    fd                          # alternative to find utility
-    jq                          # Tool to work with json
-    nmap                        # Network Discovery tool
-    glow                        # CLI markdown previewer
-    eza                         # Better ls (fork of exa, which is no longer maintained)
-    htop                        # System monitor
-    btop                        # Better htop
-    ripgrep                     # Grep alternative
-    yt-dlp                      # Download media from YouTube and similar
-    qrencode                    # Works with qr codes
-    scc                         # Code Complexity analysis
-    file                        # Gets file type
-    which                       # Gets executable path
-    gnused                      # GNU version of sed
-    gnutar                      # GNU version of tar
-    gawk                        # GNU version of AWK
-    imagemagick                 # Tooling to work with images
-    pdftk                       # PDF tool
-    ffmpeg                      # Audio library/tool
+    delta                           # nice diff viewer
+    fd                              # alternative to find utility
+    jq                              # Tool to work with json
+    nmap                            # Network Discovery tool
+    glow                            # CLI markdown previewer
+    eza                             # Better ls (fork of exa, which is no longer maintained)
+    htop                            # System monitor
+    btop                            # Better htop
+    ripgrep                         # Grep alternative
+    yt-dlp                          # Download media from YouTube and similar
+    qrencode                        # Works with qr codes
+    scc                             # Code Complexity analysis
+    file                            # Gets file type
+    which                           # Gets executable path
+    gnused                          # GNU version of sed
+    gnutar                          # GNU version of tar
+    gawk                            # GNU version of AWK
+    imagemagick                     # Tooling to work with images
+    pdftk                           # PDF tool
+    ffmpeg                          # Audio library/tool
     tree-sitter
-    tldr                        # too-long; didn't read versions of `man' pages
-    neovim                      # vim-derived editor
-    peco                        # interactive filtering/reading etc
+    tldr                            # too-long; didn't read versions of `man' pages
+    neovim                          # vim-derived editor
+    peco                            # interactive filtering/reading etc
     /*
     ┌─────────────────┐
     │ Developer Tools │
     └─────────────────┘
     */
-    hyperfine                   # Terminal benchmarking utility
-    wasm-pack                   # Packages an application for WASM
-    rizin                       # Fork of radare2 reverse-engineering tooling
-    qemu                        # emulator & virtualizer
-    ledger
-    ghostscript
-    poppler_utils
+    hyperfine                       # Terminal benchmarking utility
+    wasm-pack                       # Packages an application for WASM
+    rizin                           # Fork of radare2 reverse-engineering tooling
+    qemu                            # emulator & virtualizer
+    ghostscript                     # PDF Maniuplation 
+    poppler_utils                   # PDF Manipulation (Poppler)
     /*
     ┌───────────────┐
     │ Build Systems │
     └───────────────┘
     */
-    cmake
-    gnumake
-    just
+    cmake                           # Very bad but must use
+    gnumake                         # Often used badly
+    just                            # More sane makefile
 
     /*
     ┌────────────────────────────┐
     │ Programming Languages/SDKs │
     └────────────────────────────┘
     */
-    fnm # Fast Node Version Management
-    rustup # Rust management software
-    jdk21 # OpenJDK 21
-    gprolog # GNU Prolog
-    dotnet-sdk_8
-    zig zls # Zig language
+    fnm                             # Fast Node Version Management
+    rustup                          # Rust management software
+    jdk21                           # OpenJDK 21
+    gprolog                         # GNU Prolog
+    zig zls                         # Zig language
+
     /*
     ┌───────────────────────────┐
     │ Formal Verification, etc. │
     └───────────────────────────┘
     */
-    lean4
-    coq
-    coqPackages.coq-lsp
+    lean4                           # Used often for mathematics, admits Axiom K, often used with LEM
+    coq                             # Used for program formal verification, HoTT, etc.
+    coqPackages.coq-lsp             # Language server for coq
     /*
     ┌──────────┐
     │ Security │
     └──────────┘
     */
-    gnupg
+    gnupg                           # GPG!!
 
     /*
     ┌─────────────────────┐
@@ -153,15 +154,14 @@
     */
 
     # TeX
-    texlive.combined.scheme-full # TeX/LaTeX Distribution
-    texlab # tex lsp
-    tectonic # TeX build tooling
+    texlive.combined.scheme-full    # TeX/LaTeX Distribution
+    texlab                          # tex lsp
+    tectonic                        # TeX build tooling
 
     # Typst
     typst
     tinymist
     typstyle
-    # inputs.typst-lsp.packages.${system}.default
 
     # Writing LSP
     ltex-ls
@@ -172,16 +172,16 @@
     │ Misc. │
     └───────┘
     */
-    fswatch # Watch filesystem for changes
-    neofetch
-    hledger # Accounting Software
-    gping # gnu ping
+    fswatch                         # Watch filesystem for changes
+    neofetch                        # Pretty System info
+    hledger                         # Accounting Software
+    gping                           # graph ping
     /*
     ┌───────────┐
     │ Databases │
     └───────────┘
 
     */
-    sqlite # Embedded SQL Database
+    sqlite                          # Embedded SQL Database
   ];
 }
