f97732913cae5d94af8db89759a76744c83ff8ba
Mukul Agarwal <agarmukul23@gmail.com>
2024-11-20 09:24:45 -0500

    change wezterm package back

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -1,10 +1,8 @@
 {
-  pkgs,
   ...
 }: {
   programs.wezterm = {
     enable = true;
-    package = pkgs.runCommand "wezterm-0.0.0" {} "mkdir $out";
     extraConfig = builtins.readFile ./wezterm.lua; 
   };
 }
