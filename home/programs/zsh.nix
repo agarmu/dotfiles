b7a9298dce68bc9eefb353d3475411accf3e957b
Mukul Agarwal <agarmukul23@gmail.com>
2025-01-28 15:36:26 -0500

    opam

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -55,6 +55,14 @@
       if [[ -v WEZTERM_EXECUTABLE ]]; then
         export TERM=wezterm
       fi
+
+      # BEGIN opam configuration
+      # This is useful if you're using opam as it adds:
+      #   - the correct directories to the PATH
+      #   - auto-completion for the opam binary
+      # This section can be safely removed at any time if needed.
+      [[ ! -r "$HOME/opam-init/init.zsh" ]] || source "$HOME/.opam/opam-init/init.zsh" > /dev/null 2> /dev/null
+      # END opam configuration
     '';
   };
 }
