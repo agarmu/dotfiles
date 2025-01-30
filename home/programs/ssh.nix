f9b64e62e4d22b04fbc803ebf2ad96ea39130f35
Mukul Agarwal <agarmukul23@gmail.com>
2025-01-30 15:42:22 -0500

    add borg machines to ssh config

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -55,9 +55,124 @@
         user = "agarw396";
         forwardAgent = true;
       };
-      "anvil" = {
-        hostname = "anvil.rcac.purdue.edu";
-        user = "x-magarwal";
+      "borg01" = {
+        hostname = "borg01.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg02" = {
+        hostname = "borg02.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg03" = {
+        hostname = "borg03.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg04" = {
+        hostname = "borg04.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg05" = {
+        hostname = "borg05.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg06" = {
+        hostname = "borg06.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg07" = {
+        hostname = "borg07.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg08" = {
+        hostname = "borg08.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg09" = {
+        hostname = "borg09.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg10" = {
+        hostname = "borg10.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg11" = {
+        hostname = "borg11.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg12" = {
+        hostname = "borg12.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg13" = {
+        hostname = "borg13.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg14" = {
+        hostname = "borg14.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg15" = {
+        hostname = "borg15.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg16" = {
+        hostname = "borg16.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg17" = {
+        hostname = "borg17.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg18" = {
+        hostname = "borg18.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg19" = {
+        hostname = "borg19.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg20" = {
+        hostname = "borg20.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg21" = {
+        hostname = "borg21.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg22" = {
+        hostname = "borg22.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg23" = {
+        hostname = "borg23.cs.purdue.edu";
+        user = "agarw396";
+        forwardAgent = true;
+      };
+      "borg24" = {
+        hostname = "borg24.cs.purdue.edu";
+        user = "agarw396";
         forwardAgent = true;
       };
     };
