7ee8eb725080d209e1c71b6eb664fc0df82aeabd
Mukul Agarwal <agarmukul23@gmail.com>
2025-02-12 14:19:14 -0500

    fix ssh

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -2,13 +2,12 @@
   purdueUsername = "agarw396";
   purdueCSSystem = sn: {
     name = "${sn}";
-    value =
-      {
-        hostname = "${sn}.cs.purdue.edu";
-        user = purdueUsername;
-        forwardAgent = true;
-        proxyJump = "${purdueUsername}@data.cs.purdue.edu";
-      };
+    value = {
+      hostname = "${sn}.cs.purdue.edu";
+      user = purdueUsername;
+      forwardAgent = true;
+      proxyJump = "${purdueUsername}@data.cs.purdue.edu";
+    };
   };
   ServerMC = builtins.map (x: "mc${builtins.toString x}") (pkgs.lib.lists.range 17 21);
   ServerBORGA = builtins.map (x: "borg0${builtins.toString x}") (pkgs.lib.lists.range 1 9);
@@ -21,16 +20,18 @@ in {
     compression = true;
     hashKnownHosts = true;
     serverAliveCountMax = 5;
-    matchBlocks = {
-      "gh-test" = {
-        hostname = "github.com";
-        user = "git";
-      };
-      "data" = {
-        hostname = "data.cs.purdue.edu";
-        user = "agarw396";
-        forwardAgent = true;
-      };
-    } // servers;
+    matchBlocks =
+      {
+        "gh-test" = {
+          hostname = "github.com";
+          user = "git";
+        };
+        "data" = {
+          hostname = "data.cs.purdue.edu";
+          user = "agarw396";
+          forwardAgent = true;
+        };
+      }
+      // servers;
   };
 }
