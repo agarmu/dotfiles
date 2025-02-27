c26ab96696c054d0dacdf222b288870478b94bb4
Mukul Agarwal <agarmukul23@gmail.com>
2025-02-27 15:47:52 -0500

    dummy wezterm

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -1,5 +1,6 @@
-{...}: {
+{pkgs, ...}: {
   programs.wezterm = {
+    package = pkgs.runCommand "firefox-0.0.0" {} "mkdir $out";
     enable = true;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
