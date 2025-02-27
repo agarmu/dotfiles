85dc19ac4c183c157369a39807744f68187fcb4c
Mukul Agarwal <agarmukul23@gmail.com>
2025-02-27 18:30:25 -0500

    remove wezterm

--- home/programs/wezterm/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  programs.wezterm = {
-    package = pkgs.runCommand "firefox-0.0.0" {} "mkdir $out";
-    enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
-  };
-}
