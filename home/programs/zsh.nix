18d6779759aeac0c66ca094b0a4b368f3d2697be
Mukul Agarwal <agarmukul23@gmail.com>
2025-10-02 19:15:52 -0400

    conda -> mamba

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -33,21 +33,10 @@
       setopt EXTENDED_HISTORY
       eval "$(fnm env --use-on-cd)"
 
-      # >>> conda initialize >>>
-      # !! Contents within this block are managed by 'conda init' !!
-      __conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-          eval "$__conda_setup"
-      else
-          if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
-              . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
-          else
-              export PATH="/opt/homebrew/anaconda3/bin:$PATH"
-          fi
-      fi
-      unset __conda_setup
-      # <<< conda initialize <<<
-      # Add Cargo, LLVM, and Brew Prefixes to path
+      source "$HOME/miniforge3/etc/profile.d/conda.sh"
+      source "$HOME/miniforge3/etc/profile.d/mamba.sh"
+
+           # Add Cargo, LLVM, and Brew Prefixes to path
       export PATH="$HOME/.cargo/bin:$(brew --prefix)/opt/llvm/bin:$(brew --prefix)/opt/libiconv/bin:$PATH"
 
       source /Users/mukul/.ghcup/env
