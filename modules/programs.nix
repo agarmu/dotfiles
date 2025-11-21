96ff4c97b40eda109124921ce164a188b0407220
Mukul Agarwal <agarmukul23@gmail.com>
2025-11-20 20:21:15 -0500

    fix: gpg

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,7 +1,10 @@
 {...}: {
   programs.bash.enable = true;
   programs.zsh.enable = true;
-  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
   programs.info.enable = true;
   programs.man.enable = true;
 }
