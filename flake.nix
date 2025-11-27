3e0fc089d9e3268e85f868f4827a691fb493d139
Mukul Agarwal <agarmukul23@gmail.com>
2025-11-26 21:11:50 -0600

    fix: home-manager base config

--- flake.nix
+++ flake.nix
@@ -61,6 +61,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = specialArgs;
+            home-manager.backupFileExtension = "bak";
             home-manager.users.mukul = import ./home;
           }
           {
