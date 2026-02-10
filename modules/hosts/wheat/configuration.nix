b5ecd35d4243fabfab214ccdb89d052161896a7b
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 10:18:34 -0500

    setup wheat

--- modules/hosts/wheat/configuration.nix
+++ modules/hosts/wheat/configuration.nix
@@ -6,8 +6,11 @@
   flake.modules.nixos.wheat = {
     imports = with inputs.self.modules.nixos; [
       base
+      gui
+      asahi
       ./_hardware-configuration.nix
     ];
+    hardware.asahi.peripheralFirmwareDirectory = ./firmware;
     system.stateVersion = "26.05";
     networking.hostName = "wheat";
   };
