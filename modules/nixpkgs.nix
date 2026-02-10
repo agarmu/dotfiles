5622f9fb71843688b275c70891d6f9daec9cc7d3
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 03:56:38 -0500

    fix nixpkgs

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,23 +1,10 @@
-{ inputs, withSystem, ... }:
+{ inputs, ... }:
 {
   perSystem =
     { system, ... }:
     {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
-        config = {
-          /*
-            todo: add filtration...maybe via
-            module.args?
-          */
-          allowUnfreePredicate = _package: true;
-        };
       };
     };
-  flake.modules.nixos.base = nixosArgs: {
-    nixpkgs = {
-      pkgs = withSystem "aarch64-linux" (psArgs: psArgs.pkgs);
-      hostPlatform = "aarch64-linux";
-    };
-  };
 }
