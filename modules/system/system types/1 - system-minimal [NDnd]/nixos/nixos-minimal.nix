3d66644a79b979168e8bdf62950790dd4229dabd
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 07:38:58 -0500

    nix settings

--- modules/system/system types/1 - system-minimal [NDnd]/nixos/nixos-minimal.nix
+++ modules/system/system types/1 - system-minimal [NDnd]/nixos/nixos-minimal.nix
@@ -22,24 +22,21 @@
       nix.settings = {
         substituters = [
           # high priority since it's almost always used
-          "https://cache.nixos.org?priority=10"
-          "https://install.determinate.systems"
+          "https://cache.nixos.org"
           "https://nix-community.cachix.org"
         ];
 
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM"
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
 
         experimental-features = [
           "nix-command"
           "flakes"
+          "pipe-operators"
           # "allow-import-from-derivation"
         ];
-
-        download-buffer-size = 1024 * 1024 * 1024;
 
         trusted-users = [
           "root"
