61e8659f56e5b94352b7df21f51d10cff769745b
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 18:23:50 -0500

    add sorgum/homeManager

--- modules/hosts/sorghum/default.nix
+++ modules/hosts/sorghum/default.nix
@@ -11,10 +11,20 @@ in
       base
       gui
       homebrew
+      home-manager
     ];
     system.stateVersion = 6;
     networking.hostName = host;
     system.defaults.smb.NetBIOSName = host;
+
+    home-manager.users.mukul = {
+      imports = with inputs.self.modules.homeManager; [
+        base
+        dev
+        gui
+      ];
+      home.stateVersion = "26.05";
+    };
   };
   flake.darwinConfigurations = inputs.self.lib.mkDarwin "aarch64-darwin" host;
 }
