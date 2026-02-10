8f8bcee1116250961b88af11caeaa304e05bb5cf
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 07:23:10 -0500

    fully make wheat an asahi machine

--- modules/hosts/wheat [N]/users/mukul.nix
+++ modules/hosts/wheat [N]/users/mukul.nix
@@ -12,7 +12,6 @@
         with inputs.self.factory;
         [
           mukul
-
         ];
 
       # ...
--- modules/programs/cli-tools [ND]/generic.nix
+++ modules/programs/cli-tools [ND]/generic.nix
@@ -8,11 +8,42 @@ let
         nano
       ];
     };
+  # todo: please fix...
+  nixosPackages =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        acpi
+        kbd
+        alacritty
+        wl-clipboard
+        xwayland
+        nix-search
+        wget
+        rsync
+        fuzzel # launcher - wayland
+        brightnessctl
+        mako
+        swaylock
+        zed-editor
+        light
+        bitwarden-desktop
+        bitwarden-cli
+        waybar
+      ];
+      fonts.packages = with pkgs; [
+        inconsolata
+        cascadia-code
+        cm_unicode
+        lmodern
+      ];
+    };
 in
 {
   flake.modules.nixos.cli-tools = {
     imports = [
       genericPackages
+      nixosPackages
     ];
   };
 
