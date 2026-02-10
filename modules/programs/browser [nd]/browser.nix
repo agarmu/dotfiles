b4fa3b6eaf6891d62bb61e3eb3585bf11c483c35
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 06:28:41 -0500

    change browser

--- modules/programs/browser [nd]/browser.nix
+++ modules/programs/browser [nd]/browser.nix
@@ -2,20 +2,11 @@
   flake.modules.homeManager.browser =
     {
       pkgs,
-      lib,
       ...
     }:
     {
-      home.packages =
-        with pkgs;
-        [
-          firefox
-        ]
-        ++ lib.optionals (
-          stdenv.hostPlatform.system == "x86_64-linux"
-          || stdenv.hostPlatform.system == "aarch64-darwin"
-          || stdenv.hostPlatform.system == "x86_64-darwin"
-        ) [ google-chrome ]
-        ++ lib.optionals (stdenv.hostPlatform.system == "aarch64-linux") [ chromium ];
+      home.packages = with pkgs; [
+        firefox
+      ];
     };
 }
