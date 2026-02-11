df58ebf0ba93de8ded1923cb890a8b18923839c1
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 21:19:32 -0500

    add coconutbattery

--- modules/system/power.nix
+++ modules/system/power.nix
@@ -6,4 +6,14 @@
       environment.systemPackages = with pkgs; [ acpi ];
       services.tlp.enable = true;
     };
+
+  flake.modules.nixos.mobile =
+  { pkgs, ... }:
+  {
+    services.tlp.enable = true;
+  };
+  flake.modules.homeManager.mobile = {pkgs, ...}:
+  {
+    home.packages = [pkgs.coconutbattery];
+  };
 }
