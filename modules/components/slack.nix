bb7dcdd1b1f420dcbd571324369671799dfa6a42
Mukul Agarwal <vcs@agarmu.com>
2026-02-10 21:30:37 -0500

    add slack

---
+++ modules/components/slack.nix
@@ -0,0 +1,9 @@
+{lib, ...}:
+{
+  flake.modules.homeManager.office = {pkgs, ...}: let package =
+    if pkgs.stdenv.hostPlatform.isAarch64 && pkgs.stdenv.isLinux
+    then pkgs.slacky
+    else pkgs.slack; in {
+      home.packages = [ package ];
+  };
+}
