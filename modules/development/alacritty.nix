f3155d1b48981894fca3e66dde269287800e015c
Mukul Agarwal <vcs@agarmu.com>
2026-02-11 21:36:54 -0500

    opacity

--- modules/development/alacritty.nix
+++ modules/development/alacritty.nix
@@ -9,7 +9,7 @@
         theme = "catppuccin_mocha";
         settings = {
           env.TERM = "alacritty";
-          window.opacity = 0.93;
+          window.opacity = 0.75;
         };
       };
       home.packages = with pkgs; [
