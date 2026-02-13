e5e8730e2d77dfc0b4fafa7a5511a62288d3fc58
Mukul Agarwal <vcs@agarmu.com>
2026-02-12 19:06:39 -0500

    configure gpg

--- modules/components/gpg.nix
+++ modules/components/gpg.nix
@@ -1,20 +1,28 @@
-{ ... }:
+{ lib, ... }:
 {
-  flake.modules.nixos.base =
+ flake.modules.homeManager.base =
     { pkgs, ... }:
     {
-      programs.gnupg.agent = {
+      programs.gpg = {
         enable = true;
-        enableSSHSupport = true;
-        pinentryPackage = pkgs.pinentry-curses;
+      };
+      services.gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+        /* todo --- better way ? */
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        package =  lib.mkDefault pkgs.pinentry-curses;
       };
     };
-  flake.modules.homeManager.base =
-    { pkgs, ... }:
-    {
-      home.packages = with pkgs; [ pinentry-curses ];
+    flake.modules.homeManager.nixosGui = {pkgs, ...}: {
+      services.gnome-keyring = {
+        enable = true;
+        components = [ "secrets" ];
+      };
+      programs.gnupg.agent.pinentryPackage = pkgs.pinentry-gnome3;
     };
-  flake.modules.darwin.gui = {
+    flake.modules.darwin.gui = {
     homebrew.casks = [ "gpg-suite-pinentry" ];
   };
 }
