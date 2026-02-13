37695e6abacff00ce5af94b722da0619078c68d9
Mukul Agarwal <vcs@agarmu.com>
2026-02-12 23:11:58 -0500

    security fixes etc

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,18 +1,19 @@
 { lib, ... }:
 {
-  flake.modules.nixos.base = {
-
-  };
+  flake.modules.nixos.base =
+    { pkgs, ... }:
+    {
+      # TODO: add certs to blacklist
+      security.pki.certificateFiles = [ ];
+      security.pki.certificates = [ ];
+      security.pki.installCACerts = true; # (usually default)
+      environment.systemPackages = with pkgs; [ cacert ];
+    };
   flake.modules.darwin.base = {
     security.pam.services.sudo_local = {
       enable = true;
       reattach = true;
       touchIdAuth = true;
     };
-    # TODO: add certs to blacklist
-    security.pki.certificateFiles = [ ];
-    security.pki.certificates = [ ];
-    security.pki.installCACerts = true; # (usually default)
-    environment.systemPackages = with pkgs; [ cacert ];
   };
 }
