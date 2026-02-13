9efbb66784ab9a2de01b868b709a32a41590457a
Mukul Agarwal <vcs@agarmu.com>
2026-02-13 01:02:52 -0500

    fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,14 +1,35 @@
 { lib, ... }:
-{
-  flake.modules.nixos.base =
+let
+  fonts =
     { pkgs, ... }:
     {
       fonts.packages = with pkgs; [
         inconsolata
-        cascadia-code
         cm_unicode
         lmodern
         ibm-plex
+        nerd-fonts.iosevka-term
+        noto-fonts-color-emoji
       ];
     };
+in
+
+{
+  flake.modules.nixos.base = {
+    imports = [ fonts ];
+  };
+  flake.modules.darwin.base = {
+    imports = [ fonts ];
+  };
+  flake.modules.homeManager.base = {
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "IBM Plex Serif" ];
+        sansSerif = [ "IBM Plex Sans" ];
+        monospace = [ "IosevkaTerm Nerd Font" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 }
