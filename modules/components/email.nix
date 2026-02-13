a10cf9b820b3d4bf3d015fc58d55e951de97cd2d
Mukul Agarwal <vcs@agarmu.com>
2026-02-12 23:02:25 -0500

    thunderbird --> betterbird

--- modules/components/email.nix
+++ modules/components/email.nix
@@ -1,5 +1,9 @@
-{ lib, ... }:
+{ lib, inputs, ... }:
 {
+  flake-file.inputs.mio19 = {
+    url = "github:mio-19/nurpkgs";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   flake.modules.homeManager.gui =
     { pkgs, ... }:
     {
@@ -9,6 +13,7 @@
       };
       programs.thunderbird = {
         enable = true;
+        package = inputs.mio19.outputs.packages.${pkgs.stdenv.system}.betterbird;
         profiles.default = {
           isDefault = true;
         };
