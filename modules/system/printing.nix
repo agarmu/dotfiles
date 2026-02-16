2aef218125d39dfb8ede57baadb180d5b7901d0b
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 15:40:56 -0500

    add printers

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -1,6 +1,64 @@
 { lib, ... }:
+let
+  parse =
+    building: ident:
+    let stripped = lib.removePrefix (lib.toLower building) ident;
+      m = builtins.match "^([A-Za-z]*[0-9]+[A-Za-z]?)?(mfp)?(bw|co)?([0-9]+)?$" stripped;
+      in
+      if m == null then throw "Invalid: ${stripped}"
+      else let
+      room = builtins.elemAt m 0;
+      mfpTok = builtins.elemAt m 1;
+      mode = builtins.elemAt m 2;
+      in
+      {
+        inherit building ident room;
+        isColor = (mode == "co") || (mode == "col");
+        mfp = mfpTok == "mfp";
+      };
+
+  filterPurduePrinter =
+    { building, ... }: true;
+    # (mode == "bw" || mode == "co" || mode == "col") && (building == "LWSN" || building == "DSAI");
+
+  makePurduePrinter =
+    {
+      building,
+      ident,
+      room,
+      mfp,
+      isColor
+    }:
+      let
+        mode = if isColor then "CO" else "BW";
+        mfpQualifier = if mfp then "+MFP" else "";
+        desc = "${lib.toUpper building}-${lib.toUpper room}-${mode}${mfpQualifier}";
+      in
+      {
+        name = desc;
+        location = "Purdue University";
+        deviceUri = "smb://print.science.purdue.edu/${ident}?encryption=no";
+        model = "drv:///sample.drv/generic.ppd";
+      };
+in
 {
+  flake.modules.nixos.mobile = {
+    # services.avahi = {
+    #   enable = true;
+    #   nssmdns4 = true;
+    #   openFirewall = true;
+    # };
+  };
   flake.modules.nixos.office = {
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+    };
+    hardware.printers.ensurePrinters =
+      (builtins.readFile ./purdue-printers.json)
+      |> builtins.fromJSON
+      |> builtins.filter (x: (x.building == "LWSN") || (x.building == "DSAI"))
+      |> map (x: parse x.building x.id)
+      |> builtins.filter filterPurduePrinter
+      |> map makePurduePrinter;
   };
 }
