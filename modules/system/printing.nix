9a4b6d5ca5940b71ef793af081df77ed9db1602c
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:05:45 -0500

    printing

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -2,14 +2,17 @@
 let
   parse =
     building: ident:
-    let stripped = lib.removePrefix (lib.toLower building) ident;
+    let
+      stripped = lib.removePrefix (lib.toLower building) ident;
       m = builtins.match "^([A-Za-z]*[0-9]+[A-Za-z]?)?(mfp)?(bw|co)?([0-9]+)?$" stripped;
-      in
-      if m == null then throw "Invalid: ${stripped}"
-      else let
-      room = builtins.elemAt m 0;
-      mfpTok = builtins.elemAt m 1;
-      mode = builtins.elemAt m 2;
+    in
+    if m == null then
+      throw "Invalid: ${stripped}"
+    else
+      let
+        room = builtins.elemAt m 0;
+        mfpTok = builtins.elemAt m 1;
+        mode = builtins.elemAt m 2;
       in
       {
         inherit building ident room;
@@ -17,9 +20,8 @@ let
         mfp = mfpTok == "mfp";
       };
 
-  filterPurduePrinter =
-    { building, ... }: true;
-    # (mode == "bw" || mode == "co" || mode == "col") && (building == "LWSN" || building == "DSAI");
+  filterPurduePrinter = { building, ... }: true;
+  # (mode == "bw" || mode == "co" || mode == "col") && (building == "LWSN" || building == "DSAI");
 
   makePurduePrinter =
     {
@@ -27,22 +29,22 @@ let
       ident,
       room,
       mfp,
-      isColor
+      isColor,
     }:
-      let
-        mode = if isColor then "CO" else "BW";
-        mfpQualifier = if mfp then "+MFP" else "";
-        desc = "${lib.toUpper building}-${lib.toUpper room}-${mode}${mfpQualifier}";
-      in
-      {
-        name = desc;
-        location = "Purdue University";
-        deviceUri = "lpd://localhost:8631/${ident}";
-        model = "drv:///sample.drv/generic.ppd";
-        ppdOptions = {
-          "ColorMode" = if isColor then "color" else "monochrome";
-        };
+    let
+      mode = if isColor then "CO" else "BW";
+      mfpQualifier = if mfp then "+MFP" else "";
+      desc = "${lib.toUpper building}-${lib.toUpper room}-${mode}${mfpQualifier}";
+    in
+    {
+      name = desc;
+      location = "Purdue University";
+      deviceUri = "lpd://localhost:8631/${ident}";
+      model = "drv:///sample.drv/generic.ppd";
+      ppdOptions = {
+        "ColorMode" = if isColor then "color" else "monochrome";
       };
+    };
 in
 {
   flake.modules.nixos.mobile = {
@@ -52,19 +54,21 @@ in
     #   openFirewall = true;
     # };
   };
-  flake.modules.nixos.office = {pkgs, ...}: {
-    services.samba = {
-      enable = true;
+  flake.modules.nixos.office =
+    { pkgs, ... }:
+    {
+      services.samba = {
+        enable = true;
+      };
+      services.printing = {
+        enable = true;
+      };
+      hardware.printers.ensurePrinters =
+        (builtins.readFile ./purdue-printers.json)
+        |> builtins.fromJSON
+        |> builtins.filter (x: (x.building == "LWSN") || (x.building == "DSAI"))
+        |> map (x: parse x.building x.id)
+        |> builtins.filter filterPurduePrinter
+        |> map makePurduePrinter;
     };
-    services.printing = {
-      enable = true;
-    };
-    hardware.printers.ensurePrinters =
-      (builtins.readFile ./purdue-printers.json)
-      |> builtins.fromJSON
-      |> builtins.filter (x: (x.building == "LWSN") || (x.building == "DSAI"))
-      |> map (x: parse x.building x.id)
-      |> builtins.filter filterPurduePrinter
-      |> map makePurduePrinter;
-  };
 }
