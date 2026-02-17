923e28c856550b2eb02230bd1c0e94cf8ec74518
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:24:46 -0500

    more network config

--- modules/components/tailscale.nix
+++ modules/components/tailscale.nix
@@ -2,14 +2,22 @@
 {
   flake.modules.nixos.base = {
     services.tailscale = {
-      /*
-        don't override other firewall rules
-        https://nixos.wiki/wiki/Tailscale
-      */
       enable = true;
-      extraSetFlags = [ "--netfilter-mode=nodivert" ];
+      openFirewall = true;
+      useRoutingFeatures = lib.mkDefault "both";
+      extraUpFlags = "--accept-routes";
+
+      /*
+        	and don't track me
+
+                https://nixos.wiki/wiki/Tailscale
+      */
       extraDaemonFlags = [ "--no-logs-no-support" ];
     };
+
+    systemd.services.tailscaled.serviceConfig.Environment = [
+      "TS_DEBUG_FIREWALL_MODE=nftables"
+    ];
   };
   flake.modules.darwin.base = {
     services.tailscale.enable = true;
