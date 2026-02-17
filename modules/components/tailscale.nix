14f8a8f933149eaf739cf30a1bce1466828eab10
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:05:53 -0500

    more tailscale

--- modules/components/tailscale.nix
+++ modules/components/tailscale.nix
@@ -8,7 +8,7 @@
       */
       enable = true;
       extraSetFlags = [ "--netfilter-mode=nodivert" ];
-      extraDaemonFlags = ["--no-logs-no-support"];
+      extraDaemonFlags = [ "--no-logs-no-support" ];
     };
   };
   flake.modules.darwin.base = {
