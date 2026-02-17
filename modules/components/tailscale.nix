6aed1f197d472330c5353739b7e1f0c2e37e7b2a
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:01:13 -0500

    change tailscale config

--- modules/components/tailscale.nix
+++ modules/components/tailscale.nix
@@ -8,6 +8,7 @@
       */
       enable = true;
       extraSetFlags = [ "--netfilter-mode=nodivert" ];
+      extraDaemonFlags = ["--no-logs-no-support"];
     };
   };
   flake.modules.darwin.base = {
