4fe8d6ce01ea6c1aef650df0326609d45dbae7d3
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:24:46 -0500

    more network config

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -14,6 +14,11 @@
       networking.networkmanager = {
         enable = true;
         wifi.backend = "iwd";
+	dns = "systemd-resolved";
+      };
+
+      services.resolved = {
+        enable = true;
       };
 
       networking.nftables.enable = true;
@@ -25,8 +30,5 @@
         allowedUDPPorts = [ config.services.tailscale.port ];
       };
 
-      # Configure network proxy if necessary
-      # networking.proxy.default = "http://user:password@proxy:port/";
-      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
     };
 }
