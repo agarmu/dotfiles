59dfac3ddd09b24c991b6becb6c350c606bbd355
Mukul Agarwal <vcs@agarmu.com>
2026-02-16 20:05:53 -0500

    more tailscale

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,21 +1,32 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.base = {
-    networking.wireless.iwd = {
-      enable = true;
-      settings = {
-        Network.EnableIPv6 = true;
-        Settings.AutoConnect = true;
-        General.EnableNetworkConfiguration = true;
+  flake.modules.nixos.base =
+    { pkgs, config, ... }:
+    {
+      networking.wireless.iwd = {
+        enable = true;
+        settings = {
+          Network.EnableIPv6 = true;
+          Settings.AutoConnect = true;
+          General.EnableNetworkConfiguration = true;
+        };
       };
-    };
-    networking.networkmanager = {
-      enable = true;
-      wifi.backend = "iwd";
-    };
+      networking.networkmanager = {
+        enable = true;
+        wifi.backend = "iwd";
+      };
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  };
+      networking.nftables.enable = true;
+      networking.firewall = {
+        enable = true;
+        # Always allow traffic from your Tailscale network
+        trustedInterfaces = [ "tailscale0" ];
+        # Allow the Tailscale UDP port through the firewall
+        allowedUDPPorts = [ config.services.tailscale.port ];
+      };
+
+      # Configure network proxy if necessary
+      # networking.proxy.default = "http://user:password@proxy:port/";
+      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    };
 }
