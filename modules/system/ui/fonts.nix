e94ac4cab79f9f495a9f6807a4074782e6bd79cb
Mukul Agarwal <vcs@agarmu.com>
2026-02-18 03:50:21 -0500

    format + lint

--- modules/system/ui/fonts.nix
+++ modules/system/ui/fonts.nix
@@ -15,28 +15,29 @@ let
     };
 in
 {
-  flake.modules.nixos.base = {pkgs, ...}:
-  {
-    imports = [ fonts ];
-    stylix.fonts = {
-      serif = {
-        package = pkgs.ibm-plex;
-        name = "IBM Plex Serif";
-      };
-      sansSerif = {
-        package = pkgs.ibm-plex;
-        name = "IBM Plex Sans";
-      };
-      monospace = {
-        package = pkgs.nerd-fonts.lilex;
-        name = "Lilex Nerd Font";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-color-emoji;
-        name = "Noto Color Emoji";
+  flake.modules.nixos.base =
+    { pkgs, ... }:
+    {
+      imports = [ fonts ];
+      stylix.fonts = {
+        serif = {
+          package = pkgs.ibm-plex;
+          name = "IBM Plex Serif";
+        };
+        sansSerif = {
+          package = pkgs.ibm-plex;
+          name = "IBM Plex Sans";
+        };
+        monospace = {
+          package = pkgs.nerd-fonts.lilex;
+          name = "Lilex Nerd Font";
+        };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
       };
     };
-  };
   flake.modules.darwin.base = {
     imports = [ fonts ];
   };
