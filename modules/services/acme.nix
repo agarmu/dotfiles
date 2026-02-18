e43537fbfaf8d8feb0d7fd9e9b0480434ca83153
Mukul Agarwal <vcs@agarmu.com>
2026-02-18 04:24:16 -0500

    add irc bouncer

---
+++ modules/services/acme.nix
@@ -0,0 +1,25 @@
+_: {
+  # enable only on host millet
+  flake.modules.nixos.host-millet = {
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "acme@agarmu.com";
+      certs."millet.agarmu.com" = {
+        # default is HTTP-01; nginx integration below serves the challenge
+        webroot = "/var/lib/acme/acme-challenge";
+      };
+    };
+    services.nginx = {
+      enable = true;
+      virtualHosts."millet.agarmu.com" = {
+        # makes nginx serve /.well-known/acme-challenge/*
+        enableACME = true;
+        forceSSL = true;
+      };
+    };
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+}
