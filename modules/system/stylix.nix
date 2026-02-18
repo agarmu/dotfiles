fdfd78085ba36e2db61e2b89a48e95da6dc156cf
Mukul Agarwal <vcs@agarmu.com>
2026-02-17 21:24:13 -0500

    enable stylix

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,25 +1,32 @@
 { lib, inputs, ... }:
 let
-  stylix = inputs.stylix;
+  stylix = pkgs: {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  };
 in
 {
   flake-file.inputs.stylix = {
     url = "github:nix-community/stylix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  flake.modules.nixos.base = {
-    imports = [
-      stylix.nixosModules.stylix
-    ];
-    stylix = {
-      enable = false;
-      # breaks niri
-      homeManagerIntegration.autoImport = false;
+  flake.modules.nixos.base =
+    { pkgs, ... }:
+    {
+      imports = [
+        inputs.stylix.nixosModules.stylix
+      ];
+      stylix = (stylix pkgs) // {
+        # breaks niri
+        homeManagerIntegration.autoImport = false;
+      };
     };
-  };
-  flake.modules.homeManager.base = {
-    imports = [
-      stylix.homeModules.stylix
-    ];
-  };
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      imports = [
+        inputs.stylix.homeModules.stylix
+      ];
+      stylix = (stylix pkgs);
+    };
 }
