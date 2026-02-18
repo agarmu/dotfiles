7328ec2dd0acabf3fac45e5b66aca215a0807c2e
Mukul Agarwal <vcs@agarmu.com>
2026-02-18 06:06:37 -0500

    halloy/bouncer --> thelounge

--- modules/services/irc.nix
+++ modules/services/irc.nix
@@ -3,24 +3,10 @@ _: {
   flake.modules.nixos.host-millet =
     { config, ... }:
     {
-      services.soju =
-        let
-          tlsDir = config.security.acme.certs."millet.agarmu.com".directory;
-        in
-        {
-          enable = true;
-          hostName = "millet.agarmu.com";
-          listen = [ ":6697" ];
-          tlsCertificate = "${tlsDir}/fullchain.pem";
-          tlsCertificateKey = "${tlsDir}/key.pem";
-        };
-      networking.firewall.allowedTCPPorts = [ 6697 ];
-
-      users.groups.soju = { };
-      users.users.soju = {
-        isSystemUser = true;
-        group = "soju";
-        extraGroups = [ "acme" ];
+      services.thelounge = {
+        enable = true;
+        public = true;
+        port = 9000;
       };
     };
 }
