23996b4be76bcb4df42e22b9a27154765cb16a18
Mukul Agarwal <vcs@agarmu.com>
2026-02-18 00:56:48 -0500

    ashell config

--- modules/system/desktopbar.nix
+++ modules/system/desktopbar.nix
@@ -6,6 +6,27 @@
       programs.ashell = {
         enable = true;
         systemd.enable = true;
+        settings = {
+          modules = {
+            center = [
+            ];
+            left = [
+              "Workspaces"
+              "WindowTitle"
+            ];
+            right = [
+              "SystemInfo"
+              [
+                "Clock"
+                "Privacy"
+                "Settings"
+              ]
+            ];
+          };
+          visibility_mode = {
+            visibility_mode = "MonitorSpecific";
+          };
+        };
       };
       programs.waybar = {
         enable = false;
