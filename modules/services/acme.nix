611e0b3e1b68258802fde44d953f0c4fbc69b750
Mukul Agarwal <vcs@agarmu.com>
2026-02-18 05:12:21 -0600

    dont forward

--- modules/services/acme.nix
+++ modules/services/acme.nix
@@ -7,20 +7,14 @@ _: {
       certs."millet.agarmu.com" = {
         # default is HTTP-01; nginx integration below serves the challenge
         webroot = "/var/lib/acme/acme-challenge";
-        group = "acme";
       };
     };
-    users.users.nginx.extraGroups = [ "acme" ];
     services.nginx = {
       enable = true;
       virtualHosts."millet.agarmu.com" = {
         # makes nginx serve /.well-known/acme-challenge/*
         enableACME = true;
         forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:9000";
-          proxyWebsockets = true; # if your app uses websockets
-        };
       };
     };
     networking.firewall.allowedTCPPorts = [
