00f31e25ce7ac4111eb415247d8f326b5e228070
Mukul Agarwal <vcs@agarmu.com>
2026-02-21 01:56:05 -0500

    xdg

--- modules/components/basics.nix
+++ modules/components/basics.nix
@@ -1,24 +1,9 @@
 _: {
-  flake.modules.homeManager.base =
-    { config, ... }:
-    {
-      programs.man = {
-        enable = true;
-        generateCaches = true;
-      };
-      programs.info.enable = true;
-      xdg.userDirs = {
-        enable = true;
-        createDirectories = true;
-        desktop = "${config.home.homeDirectory}/Desktop";
-        documents = "${config.home.homeDirectory}/Documents";
-        download = "${config.home.homeDirectory}/Downloads";
-        music = "${config.home.homeDirectory}/Music";
-        pictures = "${config.home.homeDirectory}/Pictures";
-        publicShare = "${config.home.homeDirectory}/Public";
-        templates = "${config.home.homeDirectory}/Templates";
-        videos = "${config.home.homeDirectory}/Videos";
-      };
-      xdg.mimeApps.enable = true;
+  flake.modules.homeManager.base = _: {
+    programs.man = {
+      enable = true;
+      generateCaches = true;
     };
+    programs.info.enable = true;
+  };
 }
---
+++ modules/components/xdg.nix
@@ -0,0 +1,26 @@
+_: {
+  flake.modules.homeManager.base =
+    { config, ... }:
+    {
+      xdg = {
+        enable = true;
+        cacheHome = "${config.home.homeDirectory}/.cache";
+        configHome = "${config.home.homeDirectory}/.config";
+        dataHome = "${config.home.homeDirectory}/.local/share";
+        stateHome = "${config.home.homeDirectory}/.local/state";
+        mimeApps.enable = true;
+        userDirs = {
+          enable = true;
+          createDirectories = true;
+          desktop = "${config.home.homeDirectory}/Desktop";
+          documents = "${config.home.homeDirectory}/Documents";
+          download = "${config.home.homeDirectory}/Downloads";
+          music = "${config.home.homeDirectory}/Music";
+          pictures = "${config.home.homeDirectory}/Pictures";
+          publicShare = "${config.home.homeDirectory}/Public";
+          templates = "${config.home.homeDirectory}/Templates";
+          videos = "${config.home.homeDirectory}/Videos";
+        };
+      };
+    };
+}
