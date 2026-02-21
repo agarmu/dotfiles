5f2f7a22eb9587b0568b4bc5f085636a2da8b144
Mukul Agarwal <vcs@agarmu.com>
2026-02-20 23:44:55 -0500

    udiskie

--- modules/components/udiskie.nix
+++ modules/components/udiskie.nix
@@ -2,7 +2,10 @@ _: {
   flake.modules.nixos.base = _: {
     services.udisks2.enable = true;
   };
-  flake.modules.homeManager.nixosGui = _: {
-    services.udiskie.enable = true;
-  };
+  flake.modules.homeManager.nixosGui =
+    { pkgs, ... }:
+    {
+      services.udiskie.enable = true;
+      home.packages = with pkgs; [ udiskie ];
+    };
 }
