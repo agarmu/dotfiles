0cac7fa34c8ef6ce030a6b4d3a7fd4d730cbb404
Mukul Agarwal <vcs@agarmu.com>
2026-02-23 19:31:00 -0500

    sioyek

--- modules/components/pdf.nix
+++ modules/components/pdf.nix
@@ -14,9 +14,14 @@ _: {
     { pkgs, ... }:
     {
       home.packages = with pkgs; [ pdfarranger ];
+      stylix.targets.sioyek.enable = false;
       programs.sioyek = {
         enable = true;
-        config.should_launch_new_window = "1";
+        config = {
+          should_launch_new_window = "1";
+          page_separator_width = "5";
+          page_separator_color = "0.9 0.9 0.9";
+        };
       };
       xdg.mimeApps.defaultApplications = {
         "application/pdf" = [ "sioyek.desktop" ];
