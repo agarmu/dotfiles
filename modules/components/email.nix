92a5856e44eee1f2623d7778194201a555c6796d
Mukul Agarwal <vcs@agarmu.com>
2026-02-24 19:45:47 -0500

    remove mio19/betterbird

--- modules/components/email.nix
+++ modules/components/email.nix
@@ -1,15 +1,10 @@
 _: {
-  flake-file.inputs.mio19 = {
-    url = "github:mio-19/nurpkgs";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
   flake.modules.homeManager.gui = _: {
     xdg.mimeApps.defaultApplications = {
       "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
       "message/rfc822" = [ "thunderbird.desktop" ];
     };
     home.packages = [
-      # inputs.mio19.packages."${pkgs.stdenv.hostPlatform.system}".betterbird
     ];
     programs.thunderbird = {
       enable = true;
