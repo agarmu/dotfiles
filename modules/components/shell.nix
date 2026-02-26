3e8c921a683f1f6a01c1656f4ac5cb370440c887
Mukul Agarwal <vcs@agarmu.com>
2026-02-25 19:42:18 -0500

    starship config

--- modules/components/shell.nix
+++ modules/components/shell.nix
@@ -38,8 +38,23 @@
       };
       programs.starship = {
         enable = true;
+        enableZshIntegration = true;
+        enableBashIntegration = true;
         enableFishIntegration = true;
         enableNushellIntegration = true;
+        settings = {
+          add_newline = true;
+          character = {
+            success_symbol = "[>](bold green)";
+            error_symbol = "[x](bold red)";
+          };
+          directory = {
+            truncation_length = 3;
+            truncate_to_repo = true;
+            style = "bold cyan";
+          };
+          cmd_duration.min_time = 2000;
+        };
       };
     };
 
