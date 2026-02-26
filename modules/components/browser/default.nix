0b2454371c65d8399f8f1ef84ee3c469fa10f27c
Mukul Agarwal <vcs@agarmu.com>
2026-02-25 22:46:18 -0500

    formatting

--- modules/components/browser/default.nix
+++ modules/components/browser/default.nix
@@ -14,31 +14,32 @@
   flake.modules.homeManager.gui =
     { pkgs, config, ... }:
     {
-    xdg.mimeApps.defaultApplications = {
-      "text/html" = [ "firefox.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/about" = [ "firefox.desktop" ];
-      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
-    };
-    stylix.targets.firefox = {
-      enable = true;
-      firefoxGnomeTheme.enable = true;
-      profileNames = [ "default" ];
-      colors.override =
-        (config.stylix.base16.mkSchemeAttrs "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml");
-    };
-    programs.firefox = {
-      enable = true;
-      profiles.default = {
-        id = 0;
-        extraConfig = ''
-          ${builtins.readFile "${inputs.betterfox}/user.js"}
-        '';
-        settings = {
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      xdg.mimeApps.defaultApplications = {
+        "text/html" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+      };
+      stylix.targets.firefox = {
+        enable = true;
+        firefoxGnomeTheme.enable = true;
+        profileNames = [ "default" ];
+        colors.override = (
+          config.stylix.base16.mkSchemeAttrs "${pkgs.base16-schemes}/share/themes/gruvbox-light-medium.yaml"
+        );
+      };
+      programs.firefox = {
+        enable = true;
+        profiles.default = {
+          id = 0;
+          extraConfig = ''
+            ${builtins.readFile "${inputs.betterfox}/user.js"}
+          '';
+          settings = {
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          };
         };
       };
     };
-  };
 }
